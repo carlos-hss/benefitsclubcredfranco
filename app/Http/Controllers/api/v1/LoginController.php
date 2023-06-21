1f0ffd55e140db7c3c0a84d3b79e74dac27c8728
Carlos Henrique <carloshenriquesantanas19@gmail.com>
2023-06-21 01:19:11 -0300

    feat: add login controller

---
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    
+        $user = User::where('email', $credentials['email'])->first();
+    
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+    
+        $token = JWTAuth::fromUser($user);
+    
+        return response()->json(['data' => ['token' => $token], 'message' => 'Login successful'], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+}
